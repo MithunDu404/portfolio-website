fac996971a34381454f7697cea94dab909a26245
Mithun Dutta <mithundu632@gmail.com>
2025-06-28 08:54:58 +0530

    updated the requirements.txt

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -70,7 +70,7 @@ async function sendMessage() {
   showTypingIndicator();
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/chat", {
+    const response = await fetch("https://portfolio-website-fvdf.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: message }),
